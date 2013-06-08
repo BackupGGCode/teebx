223cdc396670535401f9d824ad6fd8bb3777ab8b
gvallesi@gmail.com <gvallesi@gmail.com@8d7815fd-e061-b7b6-9bef-f7aeb769be1e>
2013-06-08 18:22:47 +0000

    - Add another ARM target: the Allwinner A10 SoC based Cubieboard.

---
+++ target/cubieboard.eglibc/boot.cmd
@@ -0,0 +1,12 @@
+# $Id: $
+# part of BoneOS build platform (http://www.teebx.com/)
+# Copyright(C) 2012 - 2013 Giovanni Vallesi.
+setenv console 'ttyS0,115200'
+setenv root '/dev/ram rw'
+setenv panicarg 'panic=1'
+setenv loglevel '8'
+setenv kernel 'kernel.ubi'
+setenv initramfs 'iramfs.ubi'
+setenv setargs 'setenv bootargs console=\${console} init=/init root=\${root} \${panicarg} debug loglevel=\${loglevel}'
+setenv boot_mmc 'fatload mmc 0 0x43000000 script.bin && fatload mmc 0 0x48000000 \${kernel}; fatload mmc 0 0x44000000 \${initramfs}; bootm 0x48000000 0x44000000'
+setenv bootcmd 'run setargs boot_mmc'
