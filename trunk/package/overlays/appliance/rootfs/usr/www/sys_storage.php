f514a96ec7a064b9f5644f6a553d7ad3bfc19dd6
gvallesi@gmail.com <gvallesi@gmail.com@8d7815fd-e061-b7b6-9bef-f7aeb769be1e>
2013-06-30 19:22:16 +0000

    - New additional media configuration skeleton, unfinished but committed because neeed to keep track of future changes.

---
+++ trunk/package/overlays/appliance/rootfs/usr/www/sys_storage.php
@@ -0,0 +1,232 @@
+<?php
+/*
+  $Id$
+part of BoneOS build platform (http://www.teebx.com/)
+Copyright(C) 2010 - 2013 Giovanni Vallesi (http://www.teebx.com).
+All rights reserved.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see [http://www.gnu.org/licenses/].
+
+- BoneOS source code is available via svn at [http://svn.code.sf.net/p/boneos/code/].
+- look at TeeBX website [http://www.teebx.com] to get details about license.
+*/
+
+function microtime_float ()
+{
+	list ($msec, $sec) = explode(' ', microtime());
+	$microtime = (float)$msec + (float)$sec;
+	return $microtime;
+}
+session_start();
+$_SESSION['diskedit']['token'] = uniqid(md5(microtime()), true);
+
+$start = microtime_float();
+
+require('guiconfig.inc');
+require('blockdevices.lib.php');
+require('libs-php/htmltable.class.php');
+// define some constants referenced in fbegin.inc
+define('INCLUDE_TBLSTYLE', true);
+// page title
+$pgtitle = array(gettext('System'), gettext('Storage'));
+// actual configuration reference
+$cfgPtr = &$config['system']['storage']['disk'];
+// retrieve informations about system block devices
+$disksInfo = getBlockDevices();
+getDiskUsage($disksInfo);
+$_SESSION['diskedit']['info'] = $disksInfo;
+// init a table object
+$tbl = new htmlTable('id=table01|class=report');
+// fill the table caption
+$tbl->caption(gettext('Disk Report'));
+// table section heading
+$tbl->thead();
+	$tbl->tr();
+		$tbl->th(gettext('Configured storage'), 'class=bodytitle|colspan=3');
+	$tbl->tr();
+		$tbl->th(gettext('Name'), 'class=colheader');
+		$tbl->th(gettext('Mount Point'), 'class=colheader');
+		$tbl->th(gettext('Services'), 'class=colheader');
+	//
+// section body
+$tbl->tbody();
+	// list configured devices
+	$tbl->tr();
+	if (!empty($cfgPtr))
+	{
+
+	}
+	else
+	{
+		$tbl->td(gettext('No additional storage configured.'), 'colspan=3');
+	}
+	// list available devices
+// table section heading
+$tbl->thead();
+	$tbl->tr();
+		$tbl->th(gettext('Free disks'), 'class=bodytitle|colspan=3');
+	$tbl->tr();
+		$tbl->th(gettext('Device name'), 'class=colheader');
+		$tbl->th(gettext('Size'), 'class=colheader|colspan=2');
+	//
+// section body
+$tbl->tbody();
+	$freeDisks = getFreeDisks($cfgPtr, $disksInfo);
+	if (!empty($freeDisks))
+	{
+		foreach (array_keys($freeDisks) as $key)
+		{
+			$diskEditTool = gettext('Click here to configure this disk');
+			$actionCall = "doClickAction('new', '$key', '1')";
+			$diskEditLabel = '<a class="doedit" href="#" OnClick="' . $actionCall .'" title="' . $diskEditTool . '">'
+				. '<img  src="img/add.png" alt="+">'
+				. round($disksInfo[$key]['size']/1000000)
+				.' MB</a>';
+			$noticeLabel = '';
+			if ($freeDisks[$key] > 0)
+			{
+				// this disk is already partitioned, notify the user
+				$noticeLabel = sprintf(" Notice: this disk already has %s partition(s)", $freeDisks[$key]);
+				$noticeLabel .= '&nbsp;<img  src="img/alert.png" alt="!">';
+			}
+			$tbl->tr();
+				$tbl->td($key);
+				$tbl->td($diskEditLabel . $noticeLabel, 'colspan=2');
+			//
+		}
+	}
+	else
+	{
+		$tbl->tr();
+		$tbl->td(gettext('No free disk devices.'), 'colspan=3');
+	}
+	// show system disk summary
+// section heading
+$tbl->thead();
+	$tbl->tr();
+		$tbl->th(gettext('System disk status'), 'class=bodytitle|colspan=3');
+	$tbl->tr();
+		$tbl->th(gettext('Label'), 'class=colheader');
+		$tbl->th(gettext('Mount Point'), 'class=colheader');
+		$tbl->th(gettext('Size'), 'class=colheader');
+	//
+// section body
+$tbl->tbody();
+	foreach (array_keys($disksInfo) as $devKey)
+	{
+		if (isset($disksInfo[$devKey]['__SYS_DISK__']))
+		{
+			$devFree = $disksInfo[$devKey]['size']/1000/1024;
+			// this array will old partitions end cylinder
+			$partsEndSect = array();
+			foreach (array_keys($disksInfo[$devKey]['parts']) as $partKey)
+			{
+				$partSize = '';
+				$partSizeUnits = '';
+				$partsEndSect[] = $disksInfo[$devKey]['parts'][$partKey]['end'];
+				if (isset($disksInfo[$devKey]['parts'][$partKey]['blocks-total']))
+				{
+					$partSize = round($disksInfo[$devKey]['parts'][$partKey]['blocks-total']/1024, 1);
+					$devFree = $devFree - $partSize;
+				}
+				if (is_numeric($partSize))
+				{
+					$partSizeUnits = ' MB';
+				}
+				//
+				$mountText = '';
+				if (isset($disksInfo[$devKey]['parts'][$partKey]['mountpoint']))
+				{
+					$mountText = $disksInfo[$devKey]['parts'][$partKey]['mountpoint'];
+				}
+				//
+				$labelText = '';
+				if (isset($disksInfo[$devKey]['parts'][$partKey]['label']))
+				{
+					$labelText = $disksInfo[$devKey]['parts'][$partKey]['label'];
+				}
+				//
+				$tbl->tr();
+					$tbl->td($labelText);
+					$tbl->td($mountText);
+					$tbl->td($partSize . $partSizeUnits);
+				//
+			}
+			//
+			$newPartStart = max($partsEndSect) + 1;
+			$spareSize = $devFree - BDEV_SYS_SPARESIZE;
+			if ($spareSize >= BDEV_MIN_SIZE)
+			{
+				$tbl->thead();
+				$tbl->tr();
+					$tbl->th(gettext('Device name'), 'class=colheader');
+					$tbl->th(gettext('Spare disk space'), 'class=colheader|colspan=2');
+				//
+				$diskEditTool = gettext('Click here to configure this disk');
+				$actionCall = "doClickAction('use-spare', '$devKey', '3', '$newPartStart')";
+				$diskEditLabel = '<a class="doedit" href="#" OnClick="' . $actionCall .'" title="' . $diskEditTool . '">'
+					. '<img  src="img/add.png" alt="+">'
+					. round($devFree)
+					.' MB</a>';
+				// this is the system disk, print a notice to the user
+				$noticeLabel = ' ' . gettext('Notice: this is the system disk, avoid using it for additional storage.');
+				$noticeLabel .= '&nbsp;<img  src="img/alert.png" alt="!">';
+				$tbl->tbody();
+				$tbl->tr();
+					$tbl->td($devKey);
+					$tbl->td($diskEditLabel . $noticeLabel, 'colspan=2');
+				//
+			}
+		}
+	}
+	//
+// render main layout
+require('fbegin.inc');
+// render the page content
+$tbl->renderTable();
+
+//debug
+$end = microtime_float();
+echo '<div><pre>';
+var_export($disksInfo);
+echo '</pre></div>';
+
+echo 'Script Execution Time: ' . round($end - $start, 3) . ' seconds';
+echo '';
+?>
+<script type="text/javascript">
+	function doClickAction(action, device, partnum, partstart)
+	{
+		var $jhf = jQuery.noConflict();
+		$jhf('#act').val(action);
+		$jhf('#dev').val(device);
+		$jhf('#par').val(partnum);
+		$jhf('#start').val(partstart);
+		//
+		$jhf('form').submit();
+		return false;
+	}
+</script>
+<form action="sys_storage_edit.php" method="post">
+	<input type="hidden" id="act" name="act" value="">
+	<input type="hidden" id="dev" name="dev" value="">
+	<input type="hidden" id="par" name="par" value="">
+	<input type="hidden" id="start" name="start" value="">
+	<input type="hidden" id="stk" name="stk" value="<?php echo $_SESSION['diskedit']['token']; ?>">
+</form>
+<?php
+// end layout
+require('fend.inc');
+
+?>
---
+++ trunk/package/overlays/appliance/rootfs/usr/www/sys_storage_init.php
@@ -0,0 +1,198 @@
+<?php
+/*
+  $Id$
+part of BoneOS build platform (http://www.teebx.com/)
+Copyright(C) 2010 - 2013 Giovanni Vallesi (http://www.teebx.com).
+All rights reserved.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see [http://www.gnu.org/licenses/].
+
+- BoneOS source code is available via svn at [http://svn.code.sf.net/p/boneos/code/].
+- look at TeeBX website [http://www.teebx.com] to get details about license.
+*/
+session_start();
+require('guiconfig.inc');
+require('blockdevices.lib.php');
+define('SYS_PARTCOUNT', 2); // <-- to be moved to larger visibility
+// to be returned as json
+$data = array();
+$data['retval'] = 1;
+// messages
+$debug = array();
+// errors array
+$data['errors'] = array();
+// sanity checks
+$devToPartition = null;
+$clearPartTable = null;
+$accessAllowed = false;
+if (!isset($_POST['mode']))
+	$_POST['mode'] = null;
+if (!isset($_POST['task']))
+	$_POST['task'] = null;
+if (!isset($_POST['label']))
+	$_POST['label'] = null;
+if (isset($_SESSION['diskedit']['token']))
+{
+	if (isset($_POST))
+	{
+		if ($_POST['stk'] === $_SESSION['diskedit']['token'])
+		{
+			$accessAllowed = true;
+			if (isset($_POST['dev']))
+			{
+				if (isset($_POST['part']))
+				{
+					if (($_POST['part'] >= 1) && ($_POST['part'] <= 4))
+					{
+						$newPartNum = $_POST['part'];
+						if (isset($_SESSION['diskedit']['info']))
+						{
+							$disksInfo = $_SESSION['diskedit']['info'];
+							$devToPartition = $_POST['dev'];
+							$clearPartTable = false;
+							// be paranoid, ensure not to overwrite system partitions
+							if (isset($disksInfo[$_POST['dev']]['__SYS_DISK__']))
+							{
+								if ($newPartNum <= SYS_PARTCOUNT)
+								{
+									$data['errors'][] = gettext('Overwriting partitions not allowed on: ') . $_POST['dev'];
+								}
+							}
+							else
+							{
+								$clearPartTable = true;
+							}
+							if (is_null($_POST['label']) || ($_POST['label'] == ''))
+							{
+								$data['errors'][] = gettext('Empty or missing partition label.');
+							}
+							//
+							switch ($_POST['mode'])
+							{
+								case 'new':
+								case 'use-spare':
+								case 'edit':
+									break;
+								default:
+									$data['errors'][] = gettext('Invalid or missing mode.');
+								//
+							}
+							switch ($_POST['task'])
+							{
+								case 'partinit':
+								case 'partformat':
+									break;
+								default:
+									$data['errors'][] = gettext('Invalid or missing task.');
+							}
+						}
+						else
+						{
+							$data['errors'][] = gettext('Invalid or missing disk informations.');
+						}
+					}
+					else
+					{
+						$data['errors'][] = gettext('Invalid partition number.');
+					}
+				}
+				else
+				{
+					$data['errors'][] = gettext('Missing partition number.');
+				}
+			}
+			else
+			{
+				$data['errors'][] = gettext('Missing device name.');
+			}
+		}
+	}
+}
+if (!$accessAllowed)
+{
+	$data['errors'][] = gettext('Direct access not allowed!');
+}
+if (isset($_POST['start']))
+{
+	$sectStart = $_POST['start'];
+}
+else
+{
+	$data['errors'][] = gettext('Missing partition start sector!');
+}
+if (count($data['errors']) > 0)
+{
+	die(json_encode($data));
+}
+//
+$opStatus = 0;
+$diskDev = $_POST['dev'];
+$newPart = "$diskDev$newPartNum";
+if (strpos($diskDev, '/dev/mmcblk') !== false)
+{
+	$newPart = "{$diskDev}p{$newPartNum}";
+}
+$newPartId = 'b';
+//
+if ($_POST['task'] === 'partinit')
+{
+	$data['retval'] = newPartition($diskDev, $newPartNum, $sectStart, $clearPartTable, $newPartId);
+	if ($_POST['mode'] === 'use-spare')
+	{
+		/* when initializing a new partition on system disk
+		fdisk output a warning and return 1 so we need to check
+		that the new partition exists instead of accepting raw result.
+		*/
+		exec("fdisk -l|grep '$newPart'| awk '{print \$5}'", $out);
+		if (isset($out[0]))
+		{
+			if ($out[0] == $newPartId)
+			{
+				// return integer 2 instead of 1, evaluating this we will notice the user about actions to be done.
+				$data['retval'] = 2;
+			}
+		}
+		else
+		{
+			$data['errors'][] = gettext('Something went wrong creating new partition.');
+		}
+	}
+}
+elseif ($_POST['task'] === 'partformat')
+{
+	$newLabel = $_POST['label'];
+	$data['retval'] = formatPartitionFAT32($newPart, $newLabel);
+}
+// exit
+exit(json_encode($data));
+
+if ($_POST['mode'] === 'new')
+{
+	// New partition on dedicated disk
+}
+elseif ($_POST['mode'] === 'use-spare')
+{
+	// Partitioning spare space on system device
+}
+elseif ($_POST['mode'] === 'edit')
+{
+	// Edit current settings
+}
+else
+{
+	$data['retval'] = 1;
+	die(json_encode($data));
+}
+
+?>
