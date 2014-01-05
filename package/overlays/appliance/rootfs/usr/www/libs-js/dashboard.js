641bc7d92e24982998bc411e401b7982037ace4f
gvallesi@gmail.com <gvallesi@gmail.com@8d7815fd-e061-b7b6-9bef-f7aeb769be1e>
2014-01-05 19:18:51 +0000

    - Fix: make the clock on the index page dashboard not influenced of user browser timezone, but get time separator from the browser. - Removed debug logging code from fend.inc.

--- package/overlays/appliance/rootfs/usr/www/libs-js/dashboard.js
+++ package/overlays/appliance/rootfs/usr/www/libs-js/dashboard.js
@@ -10,13 +10,13 @@ pageVars.refresh = 1000;
 
 function updateClock(step)
 {
-	var newTimeStamp = pageVars.oldTimestamp + step;
-	var currTime = new Date(newTimeStamp);
-	var currHours = currTime.getHours();
-	var currMins = currTime.getMinutes();
-	var currSecs = currTime.getSeconds();
-
+	// since we want to show server time js date object will be not used because of client time offset
+	var newTimeStamp = pageVars.oldTimestamp + (step/1000);
 	pageVars.oldTimestamp = newTimeStamp;
+
+	var currHours = Math.floor(newTimeStamp / 3600);
+	var currMins = Math.floor(newTimeStamp % 3600 / 60);
+	var currSecs = Math.floor(newTimeStamp % 3600 % 60);
 	// Pad minutes and seconds with leading zeros, if needed
 	currMins = (currMins < 10 ? '0' : '') + currMins;
 	currSecs = (currSecs < 10 ? '0' : '') + currSecs;
@@ -24,11 +24,18 @@ function updateClock(step)
 	currHours = (currHours == 0) ? '00' : currHours;
 
 	// Apply values
-	jQuery("#systime").html(currHours + ':' + currMins + ':' + currSecs);
+	jQuery("#systime").html(currHours + pageVars.timeSeparator + currMins + pageVars.timeSeparator + currSecs);
+}
+
+function getTimeSeparator()
+{
+	var tmp = new Date(0).toLocaleTimeString();
+	return tmp.substr(tmp.length-3,1);
 }
 
 jQuery(document).ready(function()
 {
+	pageVars.timeSeparator = getTimeSeparator();
 	pageVars.oldTimestamp = Number(jQuery('#timestamp').text());
 	setInterval(function() {
 		updateClock(pageVars.refresh);
