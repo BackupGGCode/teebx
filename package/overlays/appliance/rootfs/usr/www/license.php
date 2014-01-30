f17df8d2a844fec0a10d3d72044202f7d59b4be9
gvallesi@gmail.com <gvallesi@gmail.com@8d7815fd-e061-b7b6-9bef-f7aeb769be1e>
2014-01-30 14:15:58 +0000

    - UI license page update, add also agpl3 reference link. - Main UI style sheet cleanup.

--- package/overlays/appliance/rootfs/usr/www/license.php
+++ package/overlays/appliance/rootfs/usr/www/license.php
@@ -35,9 +35,14 @@ define('INCLUDE_TBLSTYLE', true);
 $pgtitle = array('License');
 include('fbegin.inc');
 ?>
-	<p><strong>The TeeBX&reg; communication platform.<br>
-	Copyright &copy; 2010 - 2013 Giovanni Vallesi (<a href="http://teebx.org/">TeeBX.org</a>).<br>
-	All rights reserved.</strong></p>
+	<p>
+		<strong>TeeBX&reg; communication platform.
+		<a href="https://www.gnu.org/licenses/agpl.html" target="_blank"><img src="img/agplv3.png" class="imgfloatrgt" alt="GNU Affero General Public License v 3"></a><br>
+		Copyright &copy; 2010 - 2014 Giovanni Vallesi (<a href="http://www.teebx.org/">TeeBX.org</a>).<br>
+		<?php echo _('All rights reserved under the terms of AGPLv3'), '.&nbsp;<span id="toggleldetails" class="clickhere">', _('Click here to'), '&nbsp;', _('show/hide details'),'</span>';?>
+		</strong>
+	</p>
+	<div id="ldetails" style="display:none;">
 	<pre>  This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU Affero General Public License as
   published by the Free Software Foundation, either version 3 of the
@@ -52,14 +57,16 @@ include('fbegin.inc');
   along with this program.  If not, see [http://www.gnu.org/licenses/].
 
   TeeBX Source code is available via svn at [http://code.google.com/p/teebx/source/checkout].</pre>
+
   <p>Because  TeeBX includes many pieces of software the above license apply only to the code written for TeeBX itself, any other software boundled inluding the one forked apply for a proper license.</p>
   <hr size="1">
 	<p>TeeBX&reg is derived from Askozia&reg;PBX (Linux based 2.0 branch, svn revision 1948) which is currently released under a more restrictive license.<br>
 	Askozia&reg;PBX is Copyright &copy; 2007-2011 tecema gmbh a.k.a. <a href="http://www.tecema.de" target="_blank">IKT</a>.</br>
 	Please read carefully their license at <a href="http://www.askozia.com/pbx-license" target="_blank">Askozia&reg; website</a>.<br>
 	Many thanks to Michael Iedema, the whole tecema staff and all of the AskoziaPBX contributors.<br>
+	</div>
 	<hr size="1">
-	<p>TeeBX&reg is also based upon/includes various software packages, listed below. A big thanks to the authors for all of their hard work!</p>
+	<p>TeeBX&reg;&nbsp;<?php echo _('is based upon/includes various software packages, listed below. A big thanks to the authors for all of their hard work!'); ?></p>
 	<div id="software_list" class="scrollable">
 	<?php
 		$dataTbl = new csvRenderer('id=table_software_list|class=report');
@@ -75,4 +82,17 @@ include('fbegin.inc');
 	?>
 	</div>
 	<div style="clear: both;"></div>
+	<script type="text/javascript">
+		jQuery('#toggleldetails').click(function()
+		{
+			if (jQuery('#ldetails').is(':hidden'))
+			{
+				jQuery('#ldetails').slideDown('fast');
+			}
+			else
+			{
+				jQuery('#ldetails').hide();
+			}
+		});
+	</script>
 <?php include("fend.inc"); ?>
