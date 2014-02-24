33302f69e8057184cd6fd7922bcc76f8b101e770
gvallesi@gmail.com <gvallesi@gmail.com@8d7815fd-e061-b7b6-9bef-f7aeb769be1e>
2014-02-24 18:52:12 +0000

    - Small refactoring: move js functions inside maint_reboot.php into libutils.js.

--- package/overlays/appliance/rootfs/usr/www/maint_reboot.php
+++ package/overlays/appliance/rootfs/usr/www/maint_reboot.php
@@ -28,6 +28,8 @@ if (file_exists('/etc/inc/appliance.lib.php'))
 {
 	include_once '/etc/inc/appliance.lib.php';
 }
+// define some constants referenced in fbegin.inc
+define('INCLUDE_JSCRIPTS', 'libutils.js');
 
 session_start();
 $_SESSION['maint-halt']['token'] = uniqid(md5(microtime()), true);
@@ -154,93 +156,6 @@ jQuery('#btn_restart, #btn_halt').click(function(elem)
 		}
 	}
 });
-
-function doExec(url, paramsQuery, evGlobal, expectValue, targetContainer, poll, newId)
-{
-	var ret;
-
-	if (newId == null)
-	{
-		var newId = appendElement(targetContainer);
-	}
-
-	return jQuery.ajax({
-		type: 'POST',
-		url: url,
-		global: evGlobal,
-		cache: false,
-		async: false,
-		timeout: 2000,
-		data: paramsQuery,
-		dataType: 'json',
-		success: function(data)
-		{
-			jQuery(newId).html(data.msg);
-			ret = data.retval;
-			if (ret == expectValue)
-			{
-				jQuery(newId).removeClass('state_wait');
-				jQuery(newId).addClass('state_done');
-			}
-			else if (ret == null)
-			{
-				jQuery(newId).removeClass('state_wait');
-				jQuery(newId).addClass('state_error');
-			}
-		},
-		complete: function(data)
-		{
-			if (poll & (ret != expectValue))
-			{
-				setTimeout(function()
-				{
-					doExec(url, paramsQuery, evGlobal, expectValue, targetContainer, poll, newId)
-				}, 5000)
-			}
-		},
-		failure: function(data)
-		{
-			jQuery(newId).html('Error!');
-			jQuery(newId).removeClass('state_wait');
-			jQuery(newId).addClass('state_error');
-		}
-	});
-}
-
-function appendElement(updateContainer)
-{
-	var newId = 'msg' + Date.now() + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-	var wait = '<div class="state_msg state_wait" id="' + newId + '"> ...</div>';
-
-
-	jQuery(updateContainer).append(wait);
-	return '#' + newId;
-}
-
-function getFieldsInParent(parentElem)
-{
-	var params = {};
-	// checkboxes only at this time
-	jQuery(parentElem + ' input:checked').each(function()
-	{
-		params[jQuery(this).attr('id')] = jQuery(this).val();
-	});
-	return params;
-}
-
-function buildParamsQuery(paramsObj)
-{
-	var params = '';
-
-	for (var property in paramsObj)
-	{
-		if (paramsObj.hasOwnProperty(property))
-		{
-			params += '&' + property + '=' + paramsObj[property];
-		}
-	}
-	return params;
-}
 </script>
 
 <?php include 'fend.inc'; ?>
