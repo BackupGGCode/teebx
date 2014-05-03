3e9645f0083aedd05ae5fac17fd45b20c8efea86
gvallesi@gmail.com <gvallesi@gmail.com@8d7815fd-e061-b7b6-9bef-f7aeb769be1e>
2014-05-03 21:55:01 +0000

    - Setup wandboard-dual.eglibc target u-boot scripts, finally the wandboard-dual now boots. Still experimental, needs testing.

--- trunk/target/wandboard-dual.eglibc/boot.cmd
+++ trunk/target/wandboard-dual.eglibc/boot.cmd
@@ -1,12 +1,19 @@
 # $Id: $
 # part of BoneOS build platform (http://www.teebx.com/)
-# Copyright(C) 2012 - 2013 Giovanni Vallesi.
-setenv console 'ttyS0,115200'
-setenv root '/dev/ram rw'
+# Copyright(C) 2012 - 2014 Giovanni Vallesi.
+setenv console 'ttymxc0,115200'
+setenv root '/dev/ram0 rw'
 setenv panicarg 'panic=1'
 setenv loglevel '8'
-setenv kernel '/boot/kernel.ubi'
-setenv initramfs '/boot/iramfs.ubi'
+setenv fdt_addr 0x18000000
+setenv loadaddr 0x12000000
+setenv initrd_addr 0x11800000
+setenv mmcdevp ${mmcdev}:${mmcpart}
+setenv image 'boot/kernel.ubi'
+setenv initrd 'boot/iramfs.ubi' 
+setenv fdt_file 'boot/dtbs/target.dtb'
 setenv setargs 'setenv bootargs console=\${console} init=/init root=\${root} \${panicarg} debug loglevel=\${loglevel}'
-setenv boot_mmc 'fatload mmc 0 0x43000000 script.bin && fatload mmc 0 0x48000000 \${kernel}; fatload mmc 0 0x44000000 \${initramfs}; bootm 0x48000000 0x44000000'
-setenv bootcmd 'run setargs boot_mmc'
+setenv load_mmc 'fatload mmc ${mmcdevp} ${loadaddr} ${image}; fatload mmc ${mmcdevp} ${initrd_addr} ${initrd}; fatload mmc ${mmcdevp} ${fdt_addr} ${fdt_file}'
+setenv doboot 'bootz ${loadaddr} ${initrd_addr} ${fdt_addr}'
+setenv bootcmd 'run setargs load_mmc doboot'
+run bootcmd
